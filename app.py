da5b271c569075ab1dfc22dd5c77a7fe98db8d14
B Harshavardhan <b.harshavardhan.1604@gmail.com>
2026-05-03 07:51:57 +0530

    redoing

--- app.py
+++ app.py
@@ -904,4 +904,4 @@ if __name__ == '__main__':
     print("\n* LifeLine AI Server is starting...")
     print(f"  Local access: http://127.0.0.1:5000")
     
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
